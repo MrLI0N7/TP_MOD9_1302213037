397b5c19c10617aacfefc10e736a7890323a0cd9
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-04-17 01:35:48 +0800

    Menambahkan API mahasiswa

---
+++ tpmodul9_1302213037/tpmodul9_1302213037/Controllers/MahasiswaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace tpmodul9_1302213037.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : Controller
+    {   
+        // membuat List of Mahasiswa yang berisi nama Mahasiswa
+        public static List<Mahasiswa> ListMhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Liyan Made Leon WAYAN", "1302213037"),
+            new Mahasiswa("Jaatsiyah Maulidina Astrianto", "1302213043"),
+            new Mahasiswa("Rihan Hidayat", "1302210028"),
+            new Mahasiswa("Hilman Fariz Hirzi","1302213092"),
+            new Mahasiswa("Putri Auliyah Rahma","1302210033"),
+
+        };
+        // Fungsi Get untuk mendapatkan semua buah yang ada di array
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return ListMhs;
+        }
+
+        // Fungsi Get apabila diminta Mahasiswa untuk dengan index tertentu
+        [HttpGet("{idx}")]
+        public Mahasiswa Get(int idx)
+        {
+            return ListMhs[idx];
+        }
+        //Fungsi Post menambahkan Mahasiswa
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa MahasiswaBaru) 
+        {
+            ListMhs.Add(MahasiswaBaru);
+            return Ok();
+        }
+        //Fungsi Delete Menghapus Mahasiswa dengan index tertentu
+        [HttpDelete("{idx}")]
+        public IActionResult Delete(int idx)
+        {
+            ListMhs.RemoveAt(idx);
+            return Ok();
+        }
+
+    }
+}
---
+++ tpmodul9_1302213037/tpmodul9_1302213037/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmodul9_1302213037
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string nim { get; set; }
+        public Mahasiswa() { }
+        public Mahasiswa(string nama, string nim)
+        {
+            this.Nama = nama;
+            this.nim = nim;
+        }
+    }
+}
